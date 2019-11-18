edfef218a12405a58aaa96a8c9b57939014025f2
Kenneylin <17721327547@163.com>
2019-11-18 21:59:00 +0800

    change

---
+++ src/component/Commit.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Commit extends Component{
+    render(){
+        return(
+            <div>Commit 123456</div>
+        );
+    }
+}
