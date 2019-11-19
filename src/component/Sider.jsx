b76c7e07a8430b7088b83c1127dd89083662f5b7
Kenneylin <17721327547@163.com>
2019-11-19 21:05:49 +0800

    change

--- src/component/Sider.jsx
+++ src/component/Sider.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-
+import '../static/css/sider.less';
 
 export default class Sider extends Component {
     render() {
+        let str=" © 2019 Science Events Ltd</br>huanhang";
         return (
-            <div>Sider</div>
+            <div className='Sider'>
+                <div>
+                    <p>International</p>
+                    <p>Scientific</p>
+                    <p>Publications</p>
+                </div>
+                <ul>
+                    <li><a href="">Our journals</a></li>
+                    <li><a href="">Seach</a></li>
+                    <li><a href="">Publication requirements</a></li>
+                    <li><a href="">Deadlines and fees</a></li>
+                    <li><a href="">Submit a manuscipt</a></li>
+                    <li><a href="">Contacts</a></li>
+                </ul>
+                <div className="copy">
+                    <div>© 2019 Science Events Ltd</div>
+                    <div>
+                        <a href="">Terms of Use</a>&nbsp;&nbsp;·&nbsp;&nbsp;
+                        <a href="">Privacy Policy</a>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
