d634e5b0f59711625b0177d79066eb805d55f8f3
Kenneylin <17721327547@163.com>
2019-11-24 19:26:36 +0800

    add new file

---
+++ src/component/Publication.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../static/css/Publication .less';
+
+export default class Publication extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Publication requirements</h1>
+                <p>All journals by <strong>International Scientific Publications</strong> are subjective to open access policy, are published under <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank" rel="nofollow">Creative Commons Attribution License</a> and are identified by <a href="http://www.issn.org/understanding-the-issn/what-is-an-issn/" target="_blank" rel="nofollow">ISSN</a>.</p>
+                <p><strong>A manuscript may be submitted only by one of its authors, who is granted the permission of all other co-authors (if present).</strong></p>
+                <p><strong>The submitted manuscripts are considered for publication if they are intended only for the relevant journal, they have not been published already, nor are they under consideration for publication in press elsewhere.</strong> They should represent accurate, original and unique information concerning topical and significant scientific problems.</p>
+                <p><strong>Every manuscript is single blind peer-reviewed by two independent experts.</strong> The outcome is critical to the publication of the manuscript. Approving a manuscript for publication does not in any way mean that the reviewers, the editorial board and/or the publisher are consent with the authors’ point of view. The editors have the right to correct the manuscripts, if necessary.</p>
+                <p><strong>Only manuscripts written in correct English are accepted.</strong> Authors are free to choose whether to use British or American style but are obliged to use one only. Switching between two styles is unacceptable.</p>
+                <p>The submitted manuscript should be <strong>between 7 and 20 pages</strong> in DOC or DOCX format (Microsoft Office Word), consistent with the <a href="https://www.scientific-publications.net/download/formatting.pdf" target="_blank">Formatting requirements</a> and the <a href="https://www.scientific-publications.net/download/harvard.pdf" target="_blank">Harvard Referencing Style</a>. The publication of any manuscript over 20 pages long is to be negotiated with the publisher. An author can have no more than two manuscripts published in the same volume.</p>
+                <p>The publisher, the members of the editorial board and reviewers do not assume any responsibility for the content of the published article, nor the consequences of its disclosure. Articles ascertained in containing mistakes, violating copyright, publishing ethics, publishing requirements or providing misleading information, shall be removed without warning and compensation.</p>
+                <p>In case the author was granted funding by or was in any way bound to a person or an organization that are believed to may have a negative effect on the represented results, he is obliged to notice this in the end of the manuscript. This applies to any other possible conflict of interests, as well.</p>
+            </div>
+        );
+    }
+}
