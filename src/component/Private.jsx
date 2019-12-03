e62cfa1adae88c7d82678a0261eb13dd256b8e27
Kenneylin <17721327547@163.com>
2019-12-03 19:53:27 +0800

    add two new page and some picutres

---
+++ src/component/Private.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import '../static/css/private.less';
+
+export default class Term extends Component {	
+
+	render () {
+		
+		return (
+            <div >
+                <h1>Privacy Policy</h1>
+
+                <p>This is the privacy policy for scientific-publications.net which is run and provided by Science Events Ltd (we, us and our). Science Events Ltd is located at Oborishte 21, 8000 Burgas, Bulgaria. We can also be contacted at <script type="text/javascript">eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%22%6d%61%69%6c%74%6f%3a%6f%66%66%69%63%65%40%73%63%69%65%6e%74%69%66%69%63%2d%70%75%62%6c%69%63%61%74%69%6f%6e%73%2e%6e%65%74%22%20%3e%6f%66%66%69%63%65%40%73%63%69%65%6e%74%69%66%69%63%2d%70%75%62%6c%69%63%61%74%69%6f%6e%73%2e%6e%65%74%3c%2f%61%3e%27%29%3b'))</script><a href="mailto:office@scientific-publications.net">office@scientific-publications.net</a>. Below you will find information on how we use your personal data, for which purposes your personal data is used, with whom it is shared and what control and information rights you may have.</p>
+
+                <p><strong>Summary of our processing activities</strong><br/>The following summary offers a quick overview of the data processing activities that are undertaken on our website.</p>
+
+                <ul className="list">
+                        <li>When you visit our website for informational reasons, only limited personal data will be processed to provide you with the website itself.</li>
+                        <li>In case you submit a manuscript, further personal data will be processed.</li>
+                        <li>Furthermore, your personal data will be used to provide you with relevant advertising for our services and products and for statistical analysis that helps us to improve our website. Additionally, we improve your website experience with third party content.</li>
+                        <li>We have implemented appropriate safeguards to secure your personal data and retain your personal data only as long as necessary.</li>
+                        <li>Under the legislation applicable to you, you may be entitled to exercise certain rights with regard to the processing of your personal data.</li>
+                </ul>
+
+                <p><strong>Information we collect</strong><br/>We collect information from you in two ways: directly from your input and automatically.</p>
+                <p>The types of personal information that we collect directly from you depend on the content and features you use and may include:</p>
+
+                <ul className="list">
+                    <li>Contact details, such as your name, email address, postal address and telephone number</li>
+                    <li>Educational, professional, and other background information, such as field of study, current position, practice and your interests</li>
+                </ul>
+                <p>We also automatically collect information about your use of this website, such as:</p>
+
+                <ul className="list">
+                    <li>Computer, device and connection information, such as IP address, browser type and version, operating system and other technical identifiers</li>
+                    <li>Usage data, such as click stream data, including date and time stamp and referring and exit pages</li>
+                </ul>
+
+                <p><strong>How we use your information</strong><br/>We use information that we collect about you or that you provide to us, including any personal information:</p>
+
+                <ul className="list">
+                    <li>To present our website and its contents to you</li>
+                    <li>To provide you with information, services, or products that you request from us</li>
+                    <li>To carry out our obligations and enforce our rights arising from any contracts entered into between you and us, including for billing and collection</li>
+                    <li>To notify you about changes to our website or any products or services we offer or provide though it</li>
+                    <li>To respond to your requests, inquiries, comments or concerns</li>
+                    <li>To conduct and administer surveys</li>
+                    <li>To provide you with relevant special offers, promotions and other information about events, services and products of ours, our affiliates and non-affiliated third parties such as societies and sponsors</li>
+                    <li>To enhance and improve our events, services and products and develop new ones</li>
+                    <li>For any other purpose with your consent</li>
+                </ul>
+
+                <p><strong>Analytics</strong><br/>We use Google Analytics software to collect information about how you use our site. We do this to help make sure the site is meeting the needs of its users and to help us make improvements. Google Analytics stores information about:</p>
+
+                <ul className="list">
+                    <li>your device and browser</li>
+                    <li>how you got to the site</li>
+                    <li>the pages you visit</li>
+                    <li>how long you spend on each page</li>
+                    <li>what you click on while you’re visiting the site</li>
+                </ul>
+
+                <p>Google has developed a browser add-on to allow users to <a href="http://tools.google.com/dlpage/gaoptout" target="_blank" rel="noreferrer">opt-out of Google Analytics</a> across all websites which use this popular analytics product.</p>
+
+                <p><strong>Cookies</strong><br/>Cookies are small text files that are stored on your browser or device by websites, apps, online media, and advertisements. We use cookies and similar technologies for purposes such as:</p>
+
+                <ul className="list">
+                    <li>Authenticating users</li>
+                    <li>Remembering user preferences and settings</li>
+                    <li>Delivering and measuring the effectiveness of advertising campaigns</li>
+                    <li>Analyzing site traffic and trends, and generally understanding the online behaviors and interests of people who interact with our services</li>
+                </ul>
+                <p>We may also allow others to provide audience measurement and analytics services for us, to serve advertisements on our behalf across the Internet, and to track and report on the performance of those advertisements. These entities may use cookies, web beacons, SDKs, and other technologies to identify your device when you visit our site and use our services, as well as when you visit other online sites and services.</p>
+                <p>For further information about cookies, including how to see what cookies have been set on your device and how to manage and delete them, you can visit <a href="http://www.allaboutcookies.org" target="_blank" rel="noreferrer">www.allaboutcookies.org</a> and <a href="http://www.youronlinechoices.eu" target="_blank" rel="noreferrer">www.youronlinechoices.eu</a>.</p>
+
+                <p><strong>Information sharing</strong><br/>We may share your personal data with third parties in the following circumstances:</p>
+                <ul className="list">
+                    <li>With your consent: We require explicit opt-in consent for the sharing of any sensitive personal information.</li>
+                    <li>For external processing (service providers): We may engage service providers, agents or contractors to provide services on our behalf, including to administer this website and services available to you. These third parties may come to access or otherwise process your personal data in the course of providing these services.</li>
+                    <li>Compliance with laws: We will share personal data with third parties if we have a good-faith belief that this is reasonably necessary to:
+                        <ul>
+                            <li>Meet any applicable law, regulation, legal process or enforceable governmental request</li>
+                            <li>Enforce applicable Terms of Use, including investigation of potential violations</li>
+                            <li>Detect, prevent or otherwise address fraud, security or technical issues</li>
+                            <li>Protect against harm to the rights, property or safety of us, our users or the public as required or permitted by law</li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <p>Some of the recipients may reside outside the EEA.</p>
+
+                <p><strong>Cross border data transfers</strong><br/>Your personal data may, throughout the course of its processing, be transferred to other countries (including countries outside the EEA) which may have different data protection standards from your country of residence. Please note that data processed in a foreign country may be subject to foreign laws and accessible to foreign governments, courts, law enforcement, and regulatory agencies. However, we will endeavor to take reasonable measures to maintain an adequate level of data protection when sharing your personal data with such countries.</p>
+
+                <p><strong>Security</strong><br/>We are committed to doing all that we can to keep your data secure. We have set up systems and processes to prevent unauthorized access or disclosure of your data - for example, we protect your data using varying levels of encryption.</p>
+
+                <p>You should bear in mind that submission of information over the internet is never entirely secure. We cannot guarantee the security of information you submit via our website whilst it is in transit over the internet and any such submission is at your own risk.</p>
+
+                <p><strong>Data retention</strong><br/>We do not keep your personal information for longer than necessary for the purposes for which we collected it. However, we may keep your information for a longer period for historical, statistical or scientific purposes with the appropriate safeguards in place.</p>
+
+                <p><strong>Children’s privacy protection</strong><br/>Our services are not designed for, or intentionally targeted at, children under 18 years of age. We do not intentionally collect or maintain data about anyone under the age of 18.</p>
+
+                <p><strong>Your rights</strong><br/>We respect your right to access and control your information, and we will respond to requests for information and, where applicable, will correct, amend, or delete your personal information.</p>
+
+                <ul className="list">
+                    <li>Access to personal information: If you request access to your personal information, we will gladly comply, subject to any relevant legal requirements and exemptions, including identity verification procedures. Before providing data to you, we will ask for proof of identity and sufficient information about your interaction with us so that we can locate any relevant data. We may also charge you a fee for providing you with a copy of your data (except where this is not permissible under local law).</li>
+                    <li>Correction and deletion: In some jurisdictions, you have the right to correct or amend your personal information if it is inaccurate or requires updating. You may also have the right to request deletion of your personal information; however, this is not always possible due to legal requirements and other obligations and factors.</li>
+                    <li>Marketing preferences: To opt out of email marketing, you can use the unsubscribe link found in the email communication you receive from us.</li>
+                </ul>
+
+                    <p>Please contact our Data Protection Officer (<script type="text/javascript">eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%22%6d%61%69%6c%74%6f%3a%64%61%74%61%70%72%6f%74%65%63%74%69%6f%6e%40%73%63%69%65%6e%74%69%66%69%63%2d%70%75%62%6c%69%63%61%74%69%6f%6e%73%2e%6e%65%74%22%20%3e%64%61%74%61%70%72%6f%74%65%63%74%69%6f%6e%40%73%63%69%65%6e%74%69%66%69%63%2d%70%75%62%6c%69%63%61%74%69%6f%6e%73%2e%6e%65%74%3c%2f%61%3e%27%29%3b'))</script><a href="mailto:dataprotection@scientific-publications.net">dataprotection@scientific-publications.net</a>) with any requests related to your personal information.</p>
+
+                    <p><strong>Changes to this policy</strong><br/>We reserve the right to change this policy from time to time by updating our website respectively. Please visit the website regularly and check our respective current privacy policy. This policy was last updated on 24 May 2018.</p>
+
+            </div>
+		);
+	}
+}
---
+++ src/component/Terms.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../static/css/terms.less';
+export default class Term extends Component {
+	render() {
+
+		return (
+			<div >
+				<h1>Terms of use</h1>
+
+				<p>Welcome to our website. These terms of use impose conditions on your access to this site, and any use you make of it, so please read them carefully. They do not alter in any way the terms or conditions of any other agreement you may have with us for other products or services.</p>
+
+				<p>By accessing, browsing, and using this site, you agree to be bound by the terms and conditions described below, and any subsequent changes to the foregoing. If you do not agree to these terms or any subsequent modification, do not access, browse or otherwise use this site.</p>
+
+				<p>The term "us" or "we" refers to the owner of the website. The term "you" refers to the user or viewer of our website.</p>
+
+				<p>The use of this website is subject to the following terms of use:</p>
+
+				<ul className="list">
+					<li>The content of the pages of this website is for your general information and use only. It is subject to change without notice;</li>
+					<li>Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law;</li>
+					<li>Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements;</li>
+					<li>This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited unless otherwise indicated;</li>
+					<li>All trademarks reproduced in this website, which are not the property of, or licensed to the operator, are acknowledged on the website;</li>
+					<li>Unauthorised use of this website may give to a claim for damages and/or be a criminal offence;</li>
+					<li>From time to time this website may also include links to other websites. These links are provided for your convenience to provide further information. They do not signify that we endorse the website(s). We have no responsibility for the content of the linked website(s);</li>
+					<li>We use cookies to ensure that we give you the best experience on our website. Cookies are small text files that are placed on your computer by websites that you visit. They are widely used in order to make websites work, or work more efficiently, as well as to provide information to the owners of the site. The cookies we use are completely safe. You can, of course, disable cookies on your computer by indicating this in the preferences or options menus in your browser. However, it is possible that some parts of our website will not operate correctly if you disable cookies;</li>
+					<li>Your use of this website and any dispute arising out of such use of the website is subject to the laws of the European Union and Bulgaria.</li>
+				</ul>
+			</div>
+		);
+	}
+}
