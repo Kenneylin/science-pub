a328e24ed15b6916613613d522d07e8e67bd7f0b
Kenneylin <17721327547@163.com>
2019-12-03 19:52:08 +0800

    add some new routes

--- src/route.js
+++ src/route.js
@@ -1,11 +1,12 @@
-import Home from './component/Home.jsx';
 import Contacts from './component/Contacts.jsx';
 import View from './view/View.jsx';
-import Publication from '../src/component/Publication';
-import Deadlines from '../src/component/Deadlines';
+import Publication from './component/Publication';
+import Deadlines from './component/Deadlines';
 import Search from './component/Search';
 import Journals from './component/Journals';
 import Submit from './component/Submit';
+import Terms from './component/Terms';
+import Private from './component/Private';
 const routes = [
 	{
 		path: '/',
@@ -45,6 +46,16 @@ const routes = [
 			{
 				path: '/submit',
 				component:Submit,
+				exact: true,
+			},
+			{
+				path: '/terms',
+				component:Terms,
+				exact: true,
+			},
+			{
+				path: '/private',
+				component:Private ,
 				exact: true,
 			},
 		],
