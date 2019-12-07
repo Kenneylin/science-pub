f831aa7591b30daef252485a45219c9cd606a10f
Kenneylin <17721327547@163.com>
2019-12-07 20:25:01 +0800

    add some interraction javascript

--- src/component/journalcontent/Agriculture.jsx
+++ src/component/journalcontent/Agriculture.jsx
@@ -5,13 +5,44 @@ import '../static/css/sider.less';
 
 export default class Agriculture extends Component {
 	
-	render () {
-		
-		return (
+	constructor(props){
+        super(props);
+        this.state={
+            extendone:false,
+            extendtwo:false,
+        }
+        this.handleClickone=this.handleClickone.bind(this);
+        this.handleClicktwo=this.handleClicktwo.bind(this);
+    }
+   
+    handleClickone(e){
+        var value = e.target.className;
+        value=="titleone"?"titleone_open":"titleone";
+        console.log(e.target.style.backgroundimage);
+        e.target.style.backgroundimage == "url(up.png)" ? "url(down.png)":"url(up.png)";
+        console.log(e.target.style.backgroundimage);
+        this.setState({extendone:!this.state.extendone});
+    }
+    handleClicktwo(e){
+        var value = e.target.className;
+        value=="titletwo"?"titletwo_open":"titletwo";
+        this.setState({extendtwo:!this.state.extendtwo})
+    }
+    render() {
+        const styletitle={
+            backgroundpositionx: '98%',
+            backgroundpositiony: '50%',
+            fontweight: 'bold',
+            backgroundimage: 'url(up.png)',
+            backgroundcolor: 'rgb(238, 238, 238)',
+        };
+        const stylecontentone={display:this.state.extendone?'block':'none'};
+        const stylecontenttwo={display:this.state.extendtwo?'block':'none'};
+        return (
             <div className='food'>
-                 <h1 style="margin: 0;">Agriculture &amp; Food</h1>   
-                 <p>ISSN 1314-8591 (online)</p>
-                 <ul class="list">
+                <h1 style={{margin: 0,}}>Agriculture &amp; Food</h1>
+                <p>ISSN 1314-8591 (online)</p>
+                <ul className="list">
                     <li><a href="">Volume 7, 2019</a></li>
                     <li><a href="">Volume 6, 2018</a></li>
                     <li><a href="">Volume 5, 2017</a></li>
@@ -20,9 +51,9 @@ export default class Agriculture extends Component {
                     <li><a href="">Volume 2, 2014</a></li>
                 </ul>
                 <p>All issues, published prior to December, 31, 2013 are moved to <a href="">Archive</a> as a result of changes in the journals structure.</p>
-                <div class="accordion">
-                    <div class="title">Topics</div>
-                    <div class="content" style="display: none;">
+                <div className="accordion">
+                    <div className="titleone" onClick={this.handleClickone} style={styletitle}>Topics</div>
+                    <div className="content" style={stylecontentone}>
 
                         <p><strong>Agriculture</strong><br />
                             Organic agriculture<br />
@@ -72,14 +103,14 @@ export default class Agriculture extends Component {
                             The partnership between Europe and farmers<br />
                             EU grants, tenders<br />
                             International training and cooperation</p>
-                        <p style="color: #900;">Note: Papers on other relevant topics are welcome too.</p>
+                        <p style={{color: '#900'}}>Note: Papers on other relevant topics are welcome too.</p>
 
                     </div>
                 </div>
 
-                <div class="accordion">
-                    <div class="title">Editorial board</div>
-                    <div class="content" style="display: none;">
+                <div className="accordion">
+                    <div className="titletwo" onClick={this.handleClicktwo}>Editorial board</div>
+                    <div className="content" style={stylecontenttwo}>
                         <p><strong>Editor in Chief</strong><br />Irfan Ozturk, Trakya Agricultural Research Institute, Turkey</p>
                         <p><strong>Editorial Board Members</strong><br />
                             Ahmed Zahran, Minia University, Egypt<br />
@@ -96,6 +127,8 @@ export default class Agriculture extends Component {
                     </div>
                 </div>
             </div>
-		);
-	}
+        );
+    }
 }
+
+
